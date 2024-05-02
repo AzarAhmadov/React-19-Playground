e19299879b35ff3e031b85ac3e1c8502ca241a12
AzarAhmadov <ehmedovh111@gmail.com>
2024-05-02 16:15:44 +0400

    Document Metadata

--- src/pages/HomeScreen/HomeScreen.tsx
+++ src/pages/HomeScreen/HomeScreen.tsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom'
 const HomeScreen: FC = () => {
     return (
         <>
+            {/* Document Metadata */}
+            <title>React 19 new features</title>
+            <meta name="description" content="This is a Home Page" />
+
             <section className='container'>
                 <h3 className='examples'> Examples <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.278 2.231a1.001 1.001 0 0 0-.64-.231H5a2 2 0 0 0-2 2v7.21a2 2 0 0 0 1.779 1.987L12 14v6a2 2 0 0 0 4 0V8l3.03 1.212a2.001 2.001 0 0 0 2.641-1.225l.113-.34a.998.998 0 0 0-.309-1.084l-5.197-4.332z"></path></svg> </h3>
                 <div className="row">
